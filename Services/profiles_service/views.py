53de6d7b07b1297d5da7f66726293ae3eb3a2369
khalilbrikTN <mohamedkhalil.brik@aucegypt.edu>
2024-11-06 05:17:02 +0200

    error fix

--- Services/profiles_service/views.py
+++ Services/profiles_service/views.py
@@ -1,7 +1,6 @@
 
 # BY : MOHAMED KHALIL BRIK
 
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
