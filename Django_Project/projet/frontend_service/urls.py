d423813e0eceeb6b99f9c0eb66ae5a0332afb730
Khalil <mohamedkhalil.brik@aucegypt.edu>
2024-11-20 11:42:02 +0200

    front update mkb

--- Django_Project/projet/frontend_service/urls.py
+++ Django_Project/projet/frontend_service/urls.py
@@ -1,8 +1,12 @@
 # frontend/urls.py
 from django.urls import path
-from .views import main_view
+from .views import main_view, contact_view, about_view, login_view, privacy_view
 
 
 urlpatterns = [
-    path('', main_view, name='main'),  
+    path('', main_view, name='main'),
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
+    path('privacy/', privacy_view, name='privacy'),
+    path('login/', login_view, name='login'),
 ]
--- Django_Project/projet/frontend_service/views.py
+++ Django_Project/projet/frontend_service/views.py
@@ -11,3 +11,15 @@ import json
 
 def main_view(request):
     return render(request, 'frontend_service/main.html')
+
+def about_view(request):
+    return render(request, 'frontend_service/about.html')
+
+def contact_view(request):
+    return render(request, 'frontend_service/contact.html')
+
+def privacy_view(request):
+    return render(request, 'frontend_service/privacy.html')
+
+def login_view(request):
+    return render(request, 'frontend_service/login.html')
