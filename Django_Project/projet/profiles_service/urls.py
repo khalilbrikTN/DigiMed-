3b52e5429af8994171208ae6fd1f12ed7e1943cf
khalilbrikTN <mohamedkhalil.brik@aucegypt.edu>
2024-11-30 12:17:22 +0200

    urls updated

--- Django_Project/projet/profiles_service/urls.py
+++ Django_Project/projet/profiles_service/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import create_patient_view, create_doctor_view
+from .views import PatientView 
 
 
 urlpatterns = [
-    path('patient/', create_patient_view, name='create_patient'),
-    path('doctor/', create_doctor_view, name='create_doctor'),
-    path('nurse/', create_doctor_view, name='create_nurse'),
-    path('pharmacist/', create_doctor_view, name='create_pharmacist'),
+    path('patient/', PatientView.create_patient_view, name='create_patient'),
+    path('doctor/', PatientView.create_doctor_view, name='create_doctor'),
+    path('nurse/', PatientView.create_doctor_view, name='create_nurse'),
+    path('pharmacist/', PatientView.create_doctor_view, name='create_pharmacist'),
 ]
 
 
